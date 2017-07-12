eb11452c2789c20516fdedfc692090bf9daec030
WilliamRoyle <roylewilliam@gmail.com>
2017-07-12 16:05:37 -0500

    Added NavigationMethods

---
+++ app/src/main/java/com/example/williamroyle/selfdrivingminicar/NavigationMethods.java
@@ -0,0 +1,18 @@
+package com.example.williamroyle.selfdrivingminicar;
+
+/**
+ * Created by WilliamRoyle on 7/12/17.
+ */
+
+public class NavigationMethods
+{
+    /*  Returns the orientation of the android phone with respect to magnetic north,
+     *  in the range [0-360).
+     *  Method stub
+     */
+    public static double phoneCompassOrientation()
+    {
+        return 0;
+    }
+}
+
